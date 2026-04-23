76dd9895b6ce766428901ceda418c02cd48c4f56
Titouan Chauché <titouanchauche123@gmail.com>
2026-04-23 10:01:05 +0200

    feat: Add app nav

---
+++ lib/screens/delivery_screen.dart
@@ -0,0 +1,14 @@
+
+import 'package:flutter/material.dart';
+
+class DeliveryScreen extends StatelessWidget {
+  const DeliveryScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(title: const Text('Delivery')),
+        body: Text("Delivery - à implémenter")
+    );
+  }
+}
---
+++ lib/screens/stats_screen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class StatsScreen extends StatelessWidget {
+  const StatsScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(title: const Text('Stats')),
+        body: Text("Stats - à implémenter")
+    );
+  }
+}
